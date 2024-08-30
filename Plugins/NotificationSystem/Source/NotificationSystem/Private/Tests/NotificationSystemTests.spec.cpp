250823b4758c74e67bb9472fda61592ff549af82
DESKTOP-PHFAK66\juanp <juanpablohdzm95@gmail.com>
2024-08-30 18:42:45 +0200

    Add more tests and check blueprint

--- Plugins/NotificationSystem/Source/NotificationSystem/Private/Tests/NotificationSystemTests.spec.cpp
+++ Plugins/NotificationSystem/Source/NotificationSystem/Private/Tests/NotificationSystemTests.spec.cpp
@@ -27,14 +27,6 @@ void AutomationSpec::Define()
 				TestEqual("Notification should not be nullptr", notification != nullptr, true);
 			});
 
-			It("handle should be valid after creation", [this]()
-			{
-				FSimpleStruct simpleStruct;
-				FNotificationHandle handle;
-				UNotification* notification = NotificationSystem->CreateNotification<FSimpleStruct>(simpleStruct, handle);
-				TestEqual("Handle should be valid", handle.IsValid(), true);
-			});
-			
 			It("notification should exist after creation", [this]()
 			{
 				FSimpleStruct simpleStruct;
@@ -43,16 +35,85 @@ void AutomationSpec::Define()
 				TestEqual("Notification should not be nullptr", notification != nullptr, true);
 				TestEqual("Exists should return true", NotificationSystem->ExistsNotification(handle), true);
 			});
+			
+			Describe("Handle", [this]()
+			{
+				It("handle should be valid after creation", [this]()
+				{
+					FSimpleStruct simpleStruct;
+					FNotificationHandle handle;
+					UNotification* notification = NotificationSystem->CreateNotification<FSimpleStruct>(simpleStruct, handle);
+					TestEqual("Handle should be valid", handle.IsValid(), true);
+				});
+				
+				It("notification should return not null with valid handle", [this]()
+				{
+					FSimpleStruct simpleStruct;
+					FNotificationHandle handle;
+					NotificationSystem->CreateNotification<FSimpleStruct>(simpleStruct, handle);
+					const UNotification* notification = NotificationSystem->GetNotification(handle);
+					TestEqual("Notification should not be nullptr", notification != nullptr, true);
+				});
 
-			It("notification should return not null with valid handle", [this]()
+				It("notification should return null with invalid handle", [this]()
+				{
+					const FNotificationHandle& handle = FNotificationHandle::InvalidHandle();
+					const UNotification* notification = NotificationSystem->GetNotification(handle);
+					TestEqual("Notification should be nullptr", notification == nullptr, true);
+				});
+			});
+		});
+
+		Describe("notification payload", [this]()
+		{
+			It("notification payload should be the same type after creation", [this]()
+			{
+				FSimpleStruct simpleStruct;
+				simpleStruct.IntProp = 10;
+				simpleStruct.StringProp = "My struct";
+				FNotificationHandle handle;
+				NotificationSystem->CreateNotification<FSimpleStruct>(simpleStruct, handle);
+				const UNotification* notification = NotificationSystem->GetNotification(handle);
+				TestEqual("notification payload should be simple struct", notification->IsPayloadOfType<FSimpleStruct>(), true);
+			});
+
+			It("notification payload should return false if pass wrong payload", [this]()
 			{
 				FSimpleStruct simpleStruct;
 				FNotificationHandle handle;
 				NotificationSystem->CreateNotification<FSimpleStruct>(simpleStruct, handle);
 				const UNotification* notification = NotificationSystem->GetNotification(handle);
-				TestEqual("Notification should not be nullptr", notification != nullptr, true);
+				TestEqual("notification payload should not be compound struct", notification->IsPayloadOfType<FCompoundStruct>(), false);
 			});
 
+			It("notification payload should not change if simple after creation", [this]()
+			{
+				FSimpleStruct simpleStruct;
+				simpleStruct.IntProp = 10;
+				simpleStruct.StringProp = "My struct";
+				FNotificationHandle handle;
+				NotificationSystem->CreateNotification<FSimpleStruct>(simpleStruct, handle);
+				const UNotification* notification = NotificationSystem->GetNotification(handle);
+				FSimpleStruct getStruct;
+				notification->GetPayload<FSimpleStruct>(getStruct);
+				TestEqual("payload values should be the same", getStruct.StringProp == simpleStruct.StringProp, true);
+			});
+
+			It("notification payload should have values even in compound structs", [this]()
+			{
+				FSimpleStruct simpleStruct;
+				simpleStruct.IntProp = 10;
+				simpleStruct.StringProp = "My struct";
+				FCompoundStruct compoundStruct;
+				compoundStruct.SimpleStructProp = simpleStruct;
+				compoundStruct.NameProp = "Compound struct";
+				FNotificationHandle handle;
+				NotificationSystem->CreateNotification<FCompoundStruct>(compoundStruct, handle);
+				const UNotification* notification = NotificationSystem->GetNotification(handle);
+				FCompoundStruct gotStruct;
+				notification->GetPayload<FCompoundStruct>(gotStruct);
+				TestEqual("payload values should be the same", gotStruct.NameProp == compoundStruct.NameProp && gotStruct.SimpleStructProp.StringProp == compoundStruct.SimpleStructProp.StringProp, true);
+			});
 		});
 
 		AfterEach([this]()
