8db6bbbe7890b1a9784ae7080f304e988f9ca44c
DESKTOP-PHFAK66\juanp <juanpablohdzm95@gmail.com>
2024-09-01 15:23:23 +0200

    Rename to noitifcation module

---
+++ Plugins/NotificationSystem/Source/PopupSystemModule/PopupSystemModule.Build.cs
@@ -0,0 +1,24 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class PopupSystemModule : ModuleRules
+{
+	public PopupSystemModule(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"NotificationSystemModule",
+			}
+		);
+			
+		
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+	}
+}
---
+++ Plugins/NotificationSystem/Source/PopupSystemModule/Private/PopupSystemModule.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright Juan Pablo Hernandez Mosti
+
+
+IMPLEMENT_MODULE(FDefaultModuleImpl, PopupSystemModule)
